ec47bb3126adce5764057d916e823d27b4a61de9
Geoffrey Dagley <gdagley@gmail.com>
2012-06-27 11:10:48 -0500

    Initial Commit

---
+++ CocoaPodExample/FlipsideViewController.h
@@ -0,0 +1,23 @@
+//
+//  FlipsideViewController.h
+//  CocoaPodExample
+//
+//  Created by Geoffrey Dagley on 6/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FlipsideViewController;
+
+@protocol FlipsideViewControllerDelegate
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller;
+@end
+
+@interface FlipsideViewController : UIViewController
+
+@property (weak, nonatomic) id <FlipsideViewControllerDelegate> delegate;
+
+- (IBAction)done:(id)sender;
+
+@end
---
+++ CocoaPodExample/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  CocoaPodExample
+//
+//  Created by Geoffrey Dagley on 6/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+@property (strong, nonatomic) UIPopoverController *flipsidePopoverController;
+
+- (IBAction)showInfo:(id)sender;
+
+@end
